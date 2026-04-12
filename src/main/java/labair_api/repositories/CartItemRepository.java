d831100e5a2d939a1153deb1072981892778944d
LP Simeon <lp.simeon@yahoo.com>
2026-04-13 01:29:51 +0200

    feat: Created CartItemRepository

---
+++ src/main/java/labair_api/repositories/CartItemRepository.java
@@ -0,0 +1,7 @@
+package labair_api.repositories;
+
+import labair_api.models.CartItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+}
