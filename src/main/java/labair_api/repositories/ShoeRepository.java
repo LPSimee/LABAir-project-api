9e563cbcc13c771a82f97f359085fff51e8078b7
LP Simeon <lp.simeon@yahoo.com>
2026-04-13 01:18:51 +0200

    feat: Created ShoeRepository with JpaRepository extended

---
+++ src/main/java/labair_api/repositories/ShoeRepository.java
@@ -0,0 +1,9 @@
+package labair_api.repositories;
+
+import labair_api.models.Shoe;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ShoeRepository extends JpaRepository<Shoe, Long> {
+}
