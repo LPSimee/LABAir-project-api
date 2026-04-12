bebfaa1cc177863ec301800fb30d7a928d8a9d05
LP Simeon <lp.simeon@yahoo.com>
2026-04-13 01:22:26 +0200

    feat: Created UserRepository

---
+++ src/main/java/labair_api/repositories/UserRepository.java
@@ -0,0 +1,7 @@
+package labair_api.repositories;
+
+import labair_api.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
